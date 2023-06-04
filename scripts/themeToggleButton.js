5c005bc596aa64a6e96acecfd89b209fbc105181
Juud <81458475+Juudini@users.noreply.github.com>
2023-06-04 20:11:07 -0300

    Code upgrade in dark mode: -Simplified code, -Rename data.

--- scripts/themeToggleButton.js
+++ scripts/themeToggleButton.js
@@ -1,35 +1,36 @@
 //~~> Botón Para darkMode<~~.
 const darkMode = () => {
-    localStorage.setItem("toggleTheme", JSON.stringify("dark"));
+    localStorage.setItem("themeMode", JSON.stringify("Dark"));
     document.body.classList.add("dark-theme");
 };
 const lightMode = () => {
-    localStorage.setItem("toggleTheme", JSON.stringify("light"));
+    localStorage.setItem("themeMode", JSON.stringify("Light"));
     document.body.classList.remove("dark-theme");
 };
+
+const isTheme = JSON.parse(localStorage.getItem("themeMode"));
+
 const isDarkTheme = () => {
-    const isTheme = JSON.parse(localStorage.getItem("toggleTheme"));
-    if (isTheme === "dark") {
+    if (isTheme === "Dark") {
         lightMode();
-    } else if (isTheme === "light") {
+    } else if (isTheme === "Light") {
         darkMode();
     } else {
         lightMode();
     }
 };
 const themeToggleButton = document.getElementById("theme-toggle-button");
-themeToggleButton.addEventListener("click", function () {
+themeToggleButton.addEventListener("click", () => {
     isDarkTheme();
 });
-window.addEventListener("DOMContentLoaded", function () {
-    const isTheme = JSON.parse(localStorage.getItem("toggleTheme"));
-    if (isTheme === "dark") {
+window.addEventListener("DOMContentLoaded", () => {
+    if (isTheme === "Dark") {
         darkMode();
-    } else if (isTheme === "light") {
+    } else if (isTheme === "Light") {
         lightMode();
     }
 });
-let keyTheme = localStorage.getItem("toggleTheme");
+let keyTheme = localStorage.getItem("themeMode");
 if (keyTheme == null) {
     lightMode();
     console.log("🙈¡Me quieren sabotear! 🙈");
